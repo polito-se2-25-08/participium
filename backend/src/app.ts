672fa367337d2f674128e65267ce335e7cc4d66e
TridelTaxis <90930919+TridelTaxis@users.noreply.github.com>
2025-11-12 16:33:51 +0100

    fixing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,7 @@ app.get("/health", (req, res) => res.send("OK"));
 app.use("/api", officerRoutes);
 
 app.listen(port, () => {
-  console.log(`Express is listening at http://${host}:${port}`);
+	console.log(`Express is listening at http://${host}:${port}`);
 });
 
 export default app;
