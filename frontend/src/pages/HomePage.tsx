a3bedd3903cc82b16f2c36a56e913104a220fda9
TridelTaxis <90930919+TridelTaxis@users.noreply.github.com>
2025-11-19 09:17:06 +0100

    Added TailwindCSS

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,18 +2,23 @@ import { Link } from "react-router-dom";
 import "./HomePage.css";
 
 export function HomePage() {
-  return (
-    <div className="home-container">
-      <h1>Welcome to Participium!</h1>
-      <p>
-        Your city, your voice — Help keep Turin clean and efficient by reporting
-        issues you see around you. Just select a spot on the map, add a short
-        description and photo, and send your report to the municipality.
-      </p>
-      <div className="home-buttons">
-        <Link to="/login"><button>Login</button></Link>
-        <Link to="/register"><button>Register</button></Link>
-      </div>
-    </div>
-  );
+	return (
+		<div className="home-containe">
+			<h1>Welcome to Participium!</h1>
+			<p>
+				Your city, your voice — Help keep Turin clean and efficient by
+				reporting issues you see around you. Just select a spot on the
+				map, add a short description and photo, and send your report to
+				the municipality.
+			</p>
+			<div className="home-buttons">
+				<Link to="/login">
+					<button>Login</button>
+				</Link>
+				<Link to="/register">
+					<button>Register</button>
+				</Link>
+			</div>
+		</div>
+	);
 }
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -4,41 +4,41 @@ import { useAuth } from "../context/authContext";
 import "./LoginPage.css";
 
 export function LoginPage() {
-  const { login } = useAuth();
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [message, setMessage] = useState("");
-  const navigate = useNavigate();
+	const { login } = useAuth();
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [message, setMessage] = useState("");
+	const navigate = useNavigate();
 
-  async function handleSubmit(e: React.FormEvent) {
-    e.preventDefault();
-    try {
-      await login(username, password);
-      setMessage("Login successful!");
-      navigate("/profile");
-    } catch (err: any) {
-      setMessage(err.message);
-    }
-  }
+	async function handleSubmit(e: React.FormEvent) {
+		e.preventDefault();
+		try {
+			await login(username, password);
+			setMessage("Login successful!");
+			navigate("/profile");
+		} catch (err: any) {
+			setMessage(err.message);
+		}
+	}
 
-  return (
-    <div className="login-container">
-      <h2>Login</h2>
-      <form className="login-form" onSubmit={handleSubmit}>
-        <input
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Username"
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-        />
-        <button type="submit">Login</button>
-      </form>
-      {message && <p>{message}</p>}
-    </div>
-  );
+	return (
+		<div className="login-container bg-black">
+			<h2>Login</h2>
+			<form className="login-form" onSubmit={handleSubmit}>
+				<input
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+					placeholder="Username"
+				/>
+				<input
+					type="password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					placeholder="Password"
+				/>
+				<button type="submit">Login</button>
+			</form>
+			{message && <p>{message}</p>}
+		</div>
+	);
 }
