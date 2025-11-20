0274401abf8b329f8262ad69b08c2e416dd612c8
TridelTaxis <matteocarrozzino001@gmail.com>
2025-11-20 10:20:44 +0100

    direcory fix

---
+++ frontend/src/maps/TempMarker.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import type { MarkerI } from "../../interfaces/components/Marker";
+import { Marker, Popup } from "react-leaflet";
+import { fetchAddressByCoordinates } from "../../action/MapAction";
+import type { LeafletMouseEvent } from "leaflet";
+
+interface TempMarkerProps {
+	tempMarker: MarkerI | null;
+	setTempMarker: React.Dispatch<React.SetStateAction<MarkerI | null>>;
+}
+export default function TempMarker({
+	tempMarker,
+	setTempMarker,
+}: TempMarkerProps) {
+	const [isLoading, setIsLoading] = useState(false);
+
+	const getAddress = async (e: LeafletMouseEvent) => {
+		setIsLoading(true);
+		const { lat, lng } = e.latlng;
+		const address = await fetchAddressByCoordinates(lat, lng);
+
+		setTempMarker((prev) => (prev ? { ...prev, adress: address } : prev));
+		setIsLoading(false);
+	};
+
+	return tempMarker === null ? null : (
+		<Marker
+			eventHandlers={{
+				click: (e) => {
+					getAddress(e);
+				},
+			}}
+			position={tempMarker.position}
+		>
+			<Popup>{isLoading ? "Loading..." : tempMarker.adress}</Popup>
+		</Marker>
+	);
+}
