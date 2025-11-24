d22d25be2b09e564be49d6cdf8fc6c37ddcdb888
TridelTaxis <90930919+TridelTaxis@users.noreply.github.com>
2025-11-24 15:07:12 +0100

    end of profile page

---
+++ frontend/src/components/containers/ProfileContentContainer.tsx
@@ -0,0 +1,14 @@
+import type { ReactNode } from "react";
+
+interface ProfileContentContainerProps {
+	children: ReactNode;
+}
+export default function ProfileContentContainer({
+	children,
+}: ProfileContentContainerProps) {
+	return (
+		<div className="flex flex-col rounded-xl shadow-xl border border-gray-600 p-8 gap-3">
+			{children}
+		</div>
+	);
+}
